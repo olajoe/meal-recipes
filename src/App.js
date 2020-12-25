a963daf8a33e209af18b9a803c5f1072a0dab67f
Jaturon Kumo <giorojoe@gmail.com>
2020-12-25 16:59:29 +0700

    add router lib and fix according to eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react'
-import { ChakraProvider } from "@chakra-ui/core"
-import { theme } from "@chakra-ui/core"
-import { merge } from "@chakra-ui/utils"
+import { ChakraProvider } from '@chakra-ui/core'
+import { theme } from '@chakra-ui/core'
+import { merge } from '@chakra-ui/utils'
 
 import Layout from './template/Layout'
 
 const customTheme = merge(theme, {
-  colors: {
-    brand: {
-      900: "#1a365d",
-      800: "#153e75",
-      700: "#2a69ac",
-    },
-  },
+	colors: {
+		brand: {
+			900: '#1a365d',
+			800: '#153e75',
+			700: '#2a69ac',
+		},
+	},
 })
 
-
-
 function App() {
-  return (
-    <ChakraProvider resetCSS theme={customTheme}>
-      <Layout/>
-    </ChakraProvider>
-  )
+	return (
+		<ChakraProvider resetCSS theme={customTheme}>
+			<Layout />
+		</ChakraProvider>
+	)
 }
 
-export default App;
+export default App
--- src/template/Layout.jsx
+++ src/template/Layout.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Flex, Box, Text, Heading } from '@chakra-ui/core'
 
 function Layout(props) {
-	return <Flex as="nav" >
-		<div> Layout page </div>
-	</Flex>
+	return (
+		<Flex as="nav" align="center" justify="center">
+			<div> Layout page </div>
+		</Flex>
+	)
 }
 
 export default Layout
