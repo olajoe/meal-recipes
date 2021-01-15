59a823be4a4bf653587bc5c2698208d61cef1614
Jaturon Kumo <giorojoe@gmail.com>
2021-01-15 21:44:56 +0700

    remove

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import { NotFoundPage } from '../components/NotFound'
-import CategoryList from '../domain/Category/CategoryList'
 
 const Home = () => <h1>Home</h1>
 const About = () => <h1>About</h1>
@@ -13,7 +12,6 @@ const routes = () => {
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
-			<Route path="/categories" component={CategoryList} />
 			<Route path="/projects" component={Project} />
 			<Route component={NotFoundPage} />
 		</Switch>
